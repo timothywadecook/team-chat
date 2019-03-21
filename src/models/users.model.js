305a97d3a5b0d4987bbbd574040eba562f95a91b
Erwins Saget <erwinsaget@gmail.com>
2019-03-20 23:26:18 -0400

    Added authentication and fleshed out user model
    At this point you should to be login/register with name, email, password fields.

---
+++ src/models/users.model.js
@@ -0,0 +1,43 @@
+// users-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const users = new mongooseClient.Schema({
+    // The user's display name
+    name: {
+      type: String,
+      required: 'Name is required',
+    },
+
+    // the users primary email for the application
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: 'Email is required.',
+      trim: true,
+    },
+
+    // The user's password (duh!)
+    password: {
+      type: String,
+      trim: true,
+      minLength: [7, 'Password must be at least 7 characters long.'],
+    },
+
+    // This is the current team id flag for the current user
+    activeTeamId: {
+      type: mongooseClient.Schema.Types.ObjectId,
+      default: null,
+    },
+
+    // This is the list of teams ids that a user is on
+    teamIds: { type: [ mongooseClient.Schema.Types.ObjectId ] },
+
+  }, {
+    timestamps: true
+  });
+
+  return mongooseClient.model('users', users);
+};
