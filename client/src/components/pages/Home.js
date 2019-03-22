daf13a47f918dfc54d8cd990e39f27421988af11
Aaron Benson <aaronbenson@Aarons-MacBook-Pro-2.local>
2019-03-22 14:07:55 -0400

    adding team page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import CreateGroup from './createGroup'
 
 function Home(props) {
   const activeTeamId = props.activeTeamId;
-  return activeTeamId ? (<h2>Home for team {activeTeamId}</h2>) : (<h2>You aint got no team</h2>)
+  return activeTeamId ? (<h2>Home for team {activeTeamId}</h2>) 
+  : <CreateGroup/>
+
 }
 
 export default Home;
---
+++ client/src/components/pages/createGroup.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBInput } from 'mdbreact';
+
+class ModalPage extends Component {
+    state = {
+        modal: false,
+        teamInput: ''
+    }
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+    handleInput = (e) => {
+        this.setState({
+          [e.target.name]: e.target.value,
+          error: null
+        })
+      }
+    
+    handleClick = (e) => {
+        
+    }
+
+    render() {
+        return (
+            <MDBContainer>
+                <h1>Please feel free to create a team or wait until someone else adds you to their team!</h1>
+                <MDBBtn onClick={this.toggle}>Create Team</MDBBtn>
+                <MDBModal isOpen={this.state.modal} toggle={this.toggle}>
+                    <MDBModalHeader toggle={this.toggle}>Please Enter a Team Name</MDBModalHeader>
+                    <MDBModalBody>
+                        <MDBInput
+                            value = {this.state.teamInput}
+                            onChange={this.handleInput}
+                            name="teamInput"
+                            type="textarea"
+                            label="Team Name"
+                            rows="1"
+                            icon="user-friends"
+                        />
+                    </MDBModalBody>
+                    <MDBModalFooter>
+                        <MDBBtn color="primary" onClick={this.handleClick}>Create Team</MDBBtn>
+                    </MDBModalFooter>
+                </MDBModal>
+            </MDBContainer>
+        );
+    }
+}
+
+export default ModalPage;
