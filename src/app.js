3d3770fa1419011f28a1f4140099828f4ce2be8f
Erwins Saget <erwinsaget@gmail.com>
2019-04-01 08:00:04 -0400

    prod files

--- src/app.js
+++ src/app.js
@@ -32,7 +32,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
-app.use('/', express.static(app.get('public')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+} else {
+  app.use('/', express.static(app.get('public')));
+}
 
 // Set up Plugins and providers
 app.configure(express.rest());
@@ -48,10 +52,12 @@ app.configure(services);
 // Set up event channels (see channels.js)
 app.configure(channels);
 
-app.get('*', (req, res) => res.sendFile('index.html', {
-  root: path.resolve(__dirname, '../public/'),
-  dotfiles: 'deny',
-}));
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => res.sendFile('index.html', {
+    root: path.resolve(__dirname, '../client/build'),
+    dotfiles: 'deny',
+  }));
+}
 
 // Configure a middleware for 404s and the error handler
 app.use(express.notFound());
