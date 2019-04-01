7cc7a5262af8779c965fe0f09ae4264a1dede7c2
Peter Jang <peter.jang@outlook.com>
2019-03-31 22:00:31 -0400

    adds frontdor logo

--- client/src/components/pages/Index.js
+++ client/src/components/pages/Index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Jumbotron } from "reactstrap";
-import logo from "./teamchatIcon.png";
+import logo from "./frontdor_logo.png";
 
 function Index() {
   return (
     <Jumbotron className="landingPageDiv">
-        <h2 className="landingPageHeader">Team Chat</h2>
         <img src={ logo } alt="Logo Placeholder" className="logoImage"/>
+        <h2 className="landingPageHeader">FrontDor</h2>
         <Link to="/login" className="authLink">
           Login
         </Link>
