0845341e5b5761c9ac909818f432220d55dece80
Erwins Saget <erwinsaget@gmail.com>
2019-04-01 08:07:14 -0400

    remove fallback route for now

--- src/app.js
+++ src/app.js
@@ -48,11 +48,6 @@ app.configure(services);
 // Set up event channels (see channels.js)
 app.configure(channels);
 
-app.get('*', (req, res) => res.sendFile('index.html', {
-  root: path.resolve(__dirname, '../public/'),
-  dotfiles: 'deny',
-}));
-
 // Configure a middleware for 404s and the error handler
 app.use(express.notFound());
 app.use(express.errorHandler({ logger }));
